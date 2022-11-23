7fc9fe8dc749659c733331fd70f52aa33afd64dc
McAlm <stefan.wiese@camunda.com>
2022-11-23 15:31:45 +0100

    added first JPA repository

---
+++ src/main/java/org/example/camunda/process/solution/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package org.example.camunda.process.solution.repository;
+
+import org.example.camunda.process.solution.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    
+}
+
